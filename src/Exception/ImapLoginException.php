8823dbf0ab0bc838727d0000762a427cc1a6201f
Quentin <quentin@2le.net>
2023-12-05 09:35:20 +0100

    Remove useless parameter

--- src/Exception/ImapLoginException.php
+++ src/Exception/ImapLoginException.php
@@ -4,7 +4,7 @@ namespace Lle\HermesBundle\Exception;
 
 class ImapLoginException extends \Exception
 {
-    public function __construct(int $id)
+    public function __construct()
     {
         parent::__construct('Unable to connect to IMAP server');
     }
