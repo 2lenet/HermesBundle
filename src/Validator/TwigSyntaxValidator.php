05f9ae022e65ea783b17e3277a4e9a67cf7a5f35
Bastien <bastien@2le.net>
2025-03-03 12:03:29 +0100

    Twig syntax validator

---
+++ src/Validator/TwigSyntaxValidator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Lle\HermesBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+use Twig\Environment;
+use Twig\Error\SyntaxError;
+
+class TwigSyntaxValidator extends ConstraintValidator
+{
+    public function __construct(
+        protected Environment $twig
+    ) {
+    }
+
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof TwigSyntax) {
+            throw new UnexpectedTypeException($constraint, TwigSyntax::class);
+        }
+
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) to take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new UnexpectedValueException($value, 'string');
+        }
+
+        try {
+            $this->twig->disableStrictVariables();
+            $template = $this->twig->createTemplate($value);
+            $this->twig->render($template);
+            $this->twig->enableStrictVariables();
+            return;
+        } catch (SyntaxError) {
+        }
+        
+        // the argument must be a string or an object implementing __toString()
+        $this->context->buildViolation($constraint->message)
+            ->addViolation();
+    }
+}
