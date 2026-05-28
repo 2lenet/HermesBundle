f88ce4c7fe920b38775c266dc14fc07355b03aee
Artahs <julien@2le.net>
2026-05-28 15:59:15 +0200

    fix: return of the configuration in the validator

--- src/Validator/RequiredFieldValidator.php
+++ src/Validator/RequiredFieldValidator.php
@@ -2,19 +2,26 @@
 
 namespace Lle\HermesBundle\Validator;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 class RequiredFieldValidator extends ConstraintValidator
 {
+    public function __construct(
+        #[Autowire(param: 'lle_hermes.translatable_mail')]
+        private bool $translatableMail = true,
+    ) {
+    }
+
     public function validate(mixed $value, Constraint $constraint): void
     {
         if (!$constraint instanceof RequiredField) {
             throw new UnexpectedTypeException($constraint, RequiredField::class);
         }
 
-        if ($value !== null && (!is_string($value) || trim($value) !== '')) {
+        if ($this->translatableMail || ($value !== null && (!is_string($value) || trim($value) !== ''))) {
             return;
         }
 
