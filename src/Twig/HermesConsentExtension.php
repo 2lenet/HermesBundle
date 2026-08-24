5eb8259727526a97cdf4ec9e0762953ac72f7a8e
Théo <habermachertheo@gmail.com>
2026-08-24 11:28:28 +0200

    ajout block pour facilite a surcharge + service gehstion actif / deactif

---
+++ src/Twig/HermesConsentExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lle\HermesBundle\Twig;
+
+use Lle\HermesBundle\Entity\Consent;
+use Lle\HermesBundle\Service\ConsentManager;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class HermesConsentExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly ConsentManager $consentManager,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('hermes_has_consent', $this->hasConsent(...)),
+        ];
+    }
+
+    public function hasConsent(string $email): bool
+    {
+        return $this->consentManager->hasConsent($email, Consent::TYPE_PIXEL);
+    }
+}
